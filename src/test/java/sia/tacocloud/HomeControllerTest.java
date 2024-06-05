c87210e52979c7b6c259c385e7ea838edde1e40c
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2024-06-05 15:47:38 +0300

    feature: add tests to the project

---
+++ src/test/java/sia/tacocloud/HomeControllerTest.java
@@ -0,0 +1,39 @@
+package sia.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import sia.tacocloud.web.HomeController;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Тест будет запущен в контексте приложения Spring MVC.
+ * Класс {@link HomeController} регистрируется в Spring MVC
+ */
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    /**
+     * Используется объект MockMvc, для того
+     * чтобы тест мог управлять фиктивным объектом.
+     */
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * Проверка домашней страницы
+     * @throws Exception исключение, которое может возникнуть
+     */
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))                     // выполнить запрос GET /
+                .andExpect(status().isOk())                     // ожидается код ответа HTTP 200
+                .andExpect(view().name("home")) // ожидается имя представления home
+                .andExpect(content().string(containsString("Welcome to...")));
+    }
+
+}
---
+++ src/test/java/sia/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,17 @@
+package sia.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    /**
+     * Проверяет успешность загрузки контекста
+     * приложения Spring
+     */
+    @Test
+    void contextLoads() {
+    }
+
+}
