fba0a656717789f350d3712069476d70720e8602
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2026-07-01 12:37:20 +0300

    feature : add a taco repository
    * Added a taco repository

---
+++ src/main/java/sia/tacocloud/data/jpa/TacoRepository.java
@@ -0,0 +1,8 @@
+package sia.tacocloud.data.jpa;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import sia.tacocloud.domain.Taco;
+
+public interface TacoRepository extends PagingAndSortingRepository<Taco, Long>, CrudRepository<Taco, Long> {
+}
