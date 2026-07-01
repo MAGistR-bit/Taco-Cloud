7811dd39cb187118425aef9b204d46765ad847b9
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2026-07-01 15:49:09 +0300

    feature : add a delete order method
    * Added a delete order method

--- src/main/java/sia/tacocloud/web/api/OrderApiController.java
+++ src/main/java/sia/tacocloud/web/api/OrderApiController.java
@@ -1,17 +1,23 @@
 package sia.tacocloud.web.api;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import sia.tacocloud.data.jpa.OrderRepository;
 import sia.tacocloud.domain.TacoOrder;
 
 import java.util.NoSuchElementException;
 
+@Slf4j
 @RestController
 @RequestMapping(path = "/api/orders", produces = MediaType.APPLICATION_JSON_VALUE)
 @CrossOrigin(origins = "http://tacocloud:8080")
@@ -59,5 +65,19 @@ public class OrderApiController {
             order.setCcCVV(patch.getCcCVV());
         }
         return orderRepository.save(order);
+    }
+
+    /**
+     * Удаляет заказ по идентификатору
+     * @param id идентификатор заказа, который нужно удалить
+     */
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @DeleteMapping(path = "/{orderId}")
+    public void deleteOrder(@PathVariable("orderId") Long id) {
+        try {
+            orderRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            log.warn("Error when deleting: {}", e.getMessage());
+        }
     }
 }
