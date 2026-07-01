f8026d3f92e0b84e791d7e7e851369f22733be9f
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2026-07-01 15:29:36 +0300

    feature : add an order api controller
    * Added an Order API controller

---
+++ src/main/java/sia/tacocloud/web/api/OrderApiController.java
@@ -0,0 +1,63 @@
+package sia.tacocloud.web.api;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sia.tacocloud.data.jpa.OrderRepository;
+import sia.tacocloud.domain.TacoOrder;
+
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping(path = "/api/orders", produces = MediaType.APPLICATION_JSON_VALUE)
+@CrossOrigin(origins = "http://tacocloud:8080")
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    public OrderApiController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    /**
+     * Частично обновляет информацию о заказе
+     * @param id идентификатор заказа
+     * @param patch информация, которую нужно обновить для заказа
+     * @return обновленный заказ тако
+     */
+    @PatchMapping(path = "/{orderId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public TacoOrder patchOrder(@PathVariable("orderId") Long id,
+                                @RequestBody TacoOrder patch) {
+
+        TacoOrder order = orderRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Order not found"));
+        if (patch.getDeliveryName() != null) {
+            order.setDeliveryName(patch.getDeliveryName());
+        }
+        if (patch.getDeliveryStreet() != null) {
+            order.setDeliveryStreet(patch.getDeliveryStreet());
+        }
+        if (patch.getDeliveryCity() != null) {
+            order.setDeliveryCity(patch.getDeliveryCity());
+        }
+        if (patch.getDeliveryState() != null) {
+            order.setDeliveryState(patch.getDeliveryState());
+        }
+        if (patch.getDeliveryZip() != null) {
+            order.setDeliveryZip(patch.getDeliveryZip());
+        }
+        if (patch.getCcNumber() != null) {
+            order.setCcNumber(patch.getCcNumber());
+        }
+        if (patch.getCcExpiration() != null) {
+            order.setCcExpiration(patch.getCcExpiration());
+        }
+        if (patch.getCcCVV() != null) {
+            order.setCcCVV(patch.getCcCVV());
+        }
+        return orderRepository.save(order);
+    }
+}
